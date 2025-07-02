c4a08e439f8f5a05ce4fa995cdf3fa7e0f94368c
Paul Tagliamonte <paultag@gmail.com>
2025-07-02 11:16:57 -0400

    whoops

--- src/async_tokio.rs
+++ src/async_tokio.rs
@@ -51,7 +51,7 @@ impl std::error::Error for Error {}
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
