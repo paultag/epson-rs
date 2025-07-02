10fd5639ab4858f52e7e4b593c043578e5bdc4c7
Paul Tagliamonte <paultag@gmail.com>
2025-07-02 11:15:27 -0400

    Fix some nits

--- src/lib.rs
+++ src/lib.rs
@@ -150,7 +150,7 @@ impl std::error::Error for Error {}
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
